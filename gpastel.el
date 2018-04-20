21b7d79530134d6a47eeb252b684f884c769d291
Damien Cassou <damien@cassou.me>
2018-04-20 08:33:04 +0200

    Release 0.3.0

--- gpastel.el
+++ gpastel.el
@@ -5,7 +5,7 @@
 ;; Author: Damien Cassou <damien@cassou.me>
 ;; Url: https://gitlab.petton.fr/DamienCassou/desktop-environment
 ;; Package-requires: ((emacs "24.3"))
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Keywords: tools
 
 ;; This program is free software; you can redistribute it and/or modify
